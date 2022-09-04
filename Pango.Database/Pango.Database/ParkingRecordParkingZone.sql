03a57a7c39587bab0bdd95c6476674a367a5308b
vitalii.deineha <deinega.vit1@gmail.com>
2022-09-05 02:04:44 +0300

    Added on delete cascade

--- Pango.Database/Pango.Database/ParkingRecordParkingZone.sql
+++ Pango.Database/Pango.Database/ParkingRecordParkingZone.sql
@@ -2,6 +2,6 @@
 (
 	[ParkingRecordId] UNIQUEIDENTIFIER NOT NULL,
 	[ParkingZoneId] UNIQUEIDENTIFIER NOT NULL,
-	CONSTRAINT [FK_ParkingRecordParkingZone_Parking_ParkingId] FOREIGN KEY ([ParkingRecordId]) REFERENCES [dbo].[Parking] ([Id]),
-	CONSTRAINT [FK_ParkingRecordParkingZone_ParkingZone_ParkingZoneId] FOREIGN KEY ([ParkingZoneId]) REFERENCES [dbo].[ParkingZone] ([Id]),
+	CONSTRAINT [FK_ParkingRecordParkingZone_Parking_ParkingId] FOREIGN KEY ([ParkingRecordId]) REFERENCES [dbo].[Parking] ([Id]) ON DELETE CASCADE,
+	CONSTRAINT [FK_ParkingRecordParkingZone_ParkingZone_ParkingZoneId] FOREIGN KEY ([ParkingZoneId]) REFERENCES [dbo].[ParkingZone] ([Id]) ON DELETE CASCADE,
 )
