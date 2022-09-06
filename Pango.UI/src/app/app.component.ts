194eaac640c9334bcb558cf61ceb08e1740d614f
vitalii.deineha <deinega.vit1@gmail.com>
2022-09-06 10:52:35 +0300

    added redirect

--- Pango.UI/src/app/app.component.ts
+++ Pango.UI/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Pango.UI';
+
+  constructor(private router: Router) {
+
+  }
+
+  btnClickCustomer =  () => {
+    this.router.navigateByUrl('/customer');
+  };
+
+  btnClickParking =  () => {
+    this.router.navigateByUrl('/parking');
+  };
 }
